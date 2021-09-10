57c38b8407cb2828df69bcdbbecf62a2a64c420c
aTNa <atay.narynov@gmail.com>
2021-09-10 14:59:47 +0600

    Updates

---
+++ precache-manifest.213bbb39d9ed8d59880e6bcad9e4d5cb.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e29498749c25b90ad09bfd1cf386f99",
+    "url": "/RedFlagsFront/index.html"
+  },
+  {
+    "revision": "a99f66082b305c41ca2c",
+    "url": "/RedFlagsFront/static/css/2.cd8905f4.chunk.css"
+  },
+  {
+    "revision": "252a752d6484a550b1d4",
+    "url": "/RedFlagsFront/static/css/main.d8918923.chunk.css"
+  },
+  {
+    "revision": "a99f66082b305c41ca2c",
+    "url": "/RedFlagsFront/static/js/2.10955468.chunk.js"
+  },
+  {
+    "revision": "5abe57561e1a10db67aa1e3620c365fc",
+    "url": "/RedFlagsFront/static/js/2.10955468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "252a752d6484a550b1d4",
+    "url": "/RedFlagsFront/static/js/main.b48cb649.chunk.js"
+  },
+  {
+    "revision": "8f6c9347325f9cf216e5",
+    "url": "/RedFlagsFront/static/js/runtime-main.145cb93f.js"
+  },
+  {
+    "revision": "9ba4b7cbe7f072ea0fe50ec0d4b6c97b",
+    "url": "/RedFlagsFront/static/media/404.9ba4b7cb.jpg"
+  },
+  {
+    "revision": "a925745ba251f066665a252601075e16",
+    "url": "/RedFlagsFront/static/media/404.a925745b.svg"
+  },
+  {
+    "revision": "192d8f70e8dbebb4b03f98db1a6434c7",
+    "url": "/RedFlagsFront/static/media/500.192d8f70.jpg"
+  },
+  {
+    "revision": "a621f33521a64bcb6649d36d99941d5f",
+    "url": "/RedFlagsFront/static/media/500.a621f335.svg"
+  },
+  {
+    "revision": "6aae84abf0090324eb1daa7eef6bfdd8",
+    "url": "/RedFlagsFront/static/media/bootstrap-5-logo.6aae84ab.svg"
+  },
+  {
+    "revision": "95abb40d53edf7f917949e8757cdf5c6",
+    "url": "/RedFlagsFront/static/media/bs5-illustrations.95abb40d.svg"
+  },
+  {
+    "revision": "ed3cd4b507f8209412bc426332f23589",
+    "url": "/RedFlagsFront/static/media/canada.ed3cd4b5.svg"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "/RedFlagsFront/static/media/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "/RedFlagsFront/static/media/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "/RedFlagsFront/static/media/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "/RedFlagsFront/static/media/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/RedFlagsFront/static/media/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "/RedFlagsFront/static/media/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "/RedFlagsFront/static/media/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "/RedFlagsFront/static/media/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "/RedFlagsFront/static/media/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/RedFlagsFront/static/media/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/RedFlagsFront/static/media/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/RedFlagsFront/static/media/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/RedFlagsFront/static/media/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/RedFlagsFront/static/media/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/RedFlagsFront/static/media/fa-solid-900.e8a427e1.woff2"
+  },
+  {
+    "revision": "935e70467bab0575ad62110dfae45476",
+    "url": "/RedFlagsFront/static/media/forgot-password.935e7046.jpg"
+  },
+  {
+    "revision": "968aaa24eeffdd779e4fc9c27c5cb730",
+    "url": "/RedFlagsFront/static/media/france.968aaa24.svg"
+  },
+  {
+    "revision": "d810f62137d64a0a58035c80489b7d54",
+    "url": "/RedFlagsFront/static/media/germany.d810f621.svg"
+  },
+  {
+    "revision": "612e617f5d72975e63e53d793d757785",
+    "url": "/RedFlagsFront/static/media/italy.612e617f.svg"
+  },
+  {
+    "revision": "1f905d23af144bc76e488f00d516de2e",
+    "url": "/RedFlagsFront/static/media/japan.1f905d23.svg"
+  },
+  {
+    "revision": "9f0623ff820f2fde19c6c5dcb3308c39",
+    "url": "/RedFlagsFront/static/media/laravel-logo.9f0623ff.svg"
+  },
+  {
+    "revision": "69f2db42fdfeb91a54fe1573b6b102c9",
+    "url": "/RedFlagsFront/static/media/lock.69f2db42.jpg"
+  },
+  {
+    "revision": "b2d9831a6290a67a05fa6d65bb67d226",
+    "url": "/RedFlagsFront/static/media/mockup-calendar-presentation.b2d9831a.png"
+  },
+  {
+    "revision": "fc6d4c754fd2b26a2e4775bc91b61bbc",
+    "url": "/RedFlagsFront/static/media/mockup-map-presentation.fc6d4c75.png"
+  },
+  {
+    "revision": "77a4c6a89e893405dd125243971c3d61",
+    "url": "/RedFlagsFront/static/media/mockup-presentation.77a4c6a8.png"
+  },
+  {
+    "revision": "00a526136ebe6ca9adbd18a0dcb9064d",
+    "url": "/RedFlagsFront/static/media/overview.00a52613.jpg"
+  },
+  {
+    "revision": "9992e0ee061c844b089dd94784cf7e9d",
+    "url": "/RedFlagsFront/static/media/profile-cover.9992e0ee.jpg"
+  },
+  {
+    "revision": "4d26e151b224eaa88df2742351073c56",
+    "url": "/RedFlagsFront/static/media/profile-picture-1.4d26e151.jpg"
+  },
+  {
+    "revision": "487133c5319652023f459ea4e3a4720a",
+    "url": "/RedFlagsFront/static/media/profile-picture-2.487133c5.jpg"
+  },
+  {
+    "revision": "9a250e5ca0df93fd467f77c4d7e3d3d4",
+    "url": "/RedFlagsFront/static/media/profile-picture-3.9a250e5c.jpg"
+  },
+  {
+    "revision": "ccd796b31c6ca0c778c6065b3b6a20b7",
+    "url": "/RedFlagsFront/static/media/profile-picture-4.ccd796b3.jpg"
+  },
+  {
+    "revision": "baf7bf753ca889c7db833fb0db47abac",
+    "url": "/RedFlagsFront/static/media/profile-picture-5.baf7bf75.jpg"
+  },
+  {
+    "revision": "9394d800b9cadc76bc0dd4ecb6f9edcf",
+    "url": "/RedFlagsFront/static/media/react-hero-logo.9394d800.svg"
+  },
+  {
+    "revision": "5e51d949230c7a92d64c73604e5a3243",
+    "url": "/RedFlagsFront/static/media/react-logo-primary.5e51d949.svg"
+  },
+  {
+    "revision": "f7d45c01a0efd126cc1030d5cf78fd84",
+    "url": "/RedFlagsFront/static/media/react-logo-transparent.f7d45c01.svg"
+  },
+  {
+    "revision": "9675648e16658f86badd668a84c7f3b7",
+    "url": "/RedFlagsFront/static/media/react-logo.9675648e.svg"
+  },
+  {
+    "revision": "99d034b6cea27baae946ce783434954f",
+    "url": "/RedFlagsFront/static/media/react-mockup.99d034b6.png"
+  },
+  {
+    "revision": "0a4cc9cd55c1c298542e559d34622a48",
+    "url": "/RedFlagsFront/static/media/reset-password.0a4cc9cd.jpg"
+  },
+  {
+    "revision": "43fe41840c256623201cc14f63415614",
+    "url": "/RedFlagsFront/static/media/settings.43fe4184.jpg"
+  },
+  {
+    "revision": "ea051702c568e9d0199b6ddc1a52fff4",
+    "url": "/RedFlagsFront/static/media/sign-in.ea051702.jpg"
+  },
+  {
+    "revision": "6774df144d28e4f7c55c1f7725823465",
+    "url": "/RedFlagsFront/static/media/sign-up.6774df14.jpg"
+  },
+  {
+    "revision": "1217b8ee169b233ae80821ff35d74509",
+    "url": "/RedFlagsFront/static/media/signin.1217b8ee.svg"
+  },
+  {
+    "revision": "bbd344a3e9c39621e69a64c02e4b11de",
+    "url": "/RedFlagsFront/static/media/themesberg-logo.bbd344a3.svg"
+  },
+  {
+    "revision": "105e5059b0997718976601d17fd2a04b",
+    "url": "/RedFlagsFront/static/media/themesberg.105e5059.svg"
+  },
+  {
+    "revision": "b98aee40114ed3eaae525ce4ec63b0c3",
+    "url": "/RedFlagsFront/static/media/transactions.b98aee40.jpg"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/RedFlagsFront/static/media/united-kingdom.e5564902.svg"
+  },
+  {
+    "revision": "36ab476e5e55f496749ee61897a9cfb5",
+    "url": "/RedFlagsFront/static/media/united-states-of-america.36ab476e.svg"
+  }
+]);
